b1d106bdffce9abfcfca6ad2f467cf07464ee035
HinagikuKatsura <893477031@qq.com>
2017-06-18 20:39:41 +0800

    no info

--- mainsite/models.py
+++ mainsite/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title=models.CharField(max_length=200) #文章的标题
+    slug=models.CharField(max_length=200)  #文章的网址
+    body=models.TextField()                #文章的内容
+    pub_date=models.DateTimeField(default=timezone.now) #发表时间
+
+
+    class Meta:
+        ordering=('-pub_date',)#按出版时间倒序排列
+
+
+    def __unicode__(self):
+        return self.title
